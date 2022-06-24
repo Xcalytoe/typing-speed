2b844c8f7c48c0537a57a82f70f6bc370b06749b
Ezejiofor Cosmas <ezejioforcosmas@gmail.com>
2022-06-24 15:05:52 +0100

    Add _document to the app

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  presets: ["next/babel"],
-  plugins: [["styled-components", { ssr: true }]],
-
   compiler: {
     styledComponents: true,
   },
