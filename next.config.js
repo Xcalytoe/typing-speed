4cd623eaa69bc2ae63a33e21af16dcf74553947a
Ezejiofor Cosmas <ezejioforcosmas@gmail.com>
2022-06-24 14:43:48 +0100

    Enable styled component on the server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  presets: ["next/babel"],
+  plugins: [["styled-components", { ssr: true }]],
+
   compiler: {
     styledComponents: true,
   },
